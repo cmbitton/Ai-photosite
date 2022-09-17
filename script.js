8c36d35d8f13ad22069b949ed1c079d1416e0160
Curtis Bitton <cmbitton@gmail.com>
2022-09-16 22:06:30 -0400

    Add button for grid options
    Added a button on the gallery
    page to adjust grid sizes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getPics() {} //just for this demo
 const imgs = document.querySelectorAll('.image-card .photo');
 const fullPage = document.querySelector('#fullpage');
-
+let imageCards = document.querySelectorAll('.image-card');
+const button2x2Grid = document.querySelector('.gridbutton2x2');
 
 imgs.forEach(img => {
   img.addEventListener('click', function() {
@@ -10,3 +11,15 @@ imgs.forEach(img => {
   });
 });
 getPics();
+
+
+button2x2Grid.addEventListener('click', function () {
+  for (let i = 0; i < imageCards.length; i++) {
+    if (imageCards[i].id !== 'image-card-2x2grid') {
+      imageCards[i].setAttribute('id', 'image-card-2x2grid');
+    }
+    else{
+      imageCards[i].removeAttribute('id', 'image-card-2x2grid');
+    }
+  }
+});
